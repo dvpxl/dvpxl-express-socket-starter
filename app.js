6c3216e77d9eba1214a1e72ff092c78ac74e2619
Jonathan Paek <jonathan@dvpxl.com>
2015-11-04 17:16:53 -0800

    adds mongodb support with sample

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var server = require('http').Server(app);  
 var bodyParser = require('body-parser');
 var io = require('socket.io')(server);
+var mongoose = require('mongoose');
 
 
 app.set('port', (process.env.PORT || 5000));
@@ -17,6 +18,71 @@ app.set('view engine', 'ejs');
 
 var routes = require('./routes.js');
     routes(app, io);
+
+
+/*
+Mongo database connection test
+*/
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback){
+	console.log('connection to database successful');
+});
+
+/*
+Mongoose simple schema
+*/
+
+var userSchema = mongoose.Schema({
+    name: String,
+    type: String,
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now },
+    meta: {
+    	votes: Number,
+    	favs: Number
+    }
+});
+
+/*
+Mongoose save middleware
+inserts the proper created/updated time
+*/
+userSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
+});
+
+
+var User = mongoose.model('User', userSchema);
+
+//creating a test user
+var user1 = new User({
+	name: 'john doe',
+	type: 'student',
+	meta: {
+		votes: 1,
+		favs: 1
+	}
+});
+
+//output
+console.log('Created User: \n' + user1);
+
+//save user
+user1.save(function (err, user){
+	if(err) return console.error(err);
+	console.info("User Saved to Database\n" + user);
+});	
+
 
 /***
 * Socket handling
@@ -40,7 +106,6 @@ io.on('connection', function(client) {
 		console.log('emitting heartbeat' + random);
 	}, 2000);
 });
-
 
 
 server.listen(app.get('port'), function(){
